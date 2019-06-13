2fc8c9ab22e41945e900a994467914150dbd1567
tobia <wl850932@dal.ca>
2019-06-12 21:41:43 -0300

    route set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AvailableRoomComponent } from './available-room/available-room.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BookingsComponent } from './bookings/bookings.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+    AvailableRoomComponent,
+    ProfileComponent,
+    BookingsComponent,
+    FeedbackComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
