613e2e6f58c1f93ed284720a6e99482ceae4e6c1
Agbola Iseoluwatobi <is814889@dal.ca>
2019-06-12 21:41:43 -0300

    Route set-up

---
+++ src/app/available-room/available-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-available-room',
+  templateUrl: './available-room.component.html',
+  styleUrls: ['./available-room.component.css']
+})
+export class AvailableRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
