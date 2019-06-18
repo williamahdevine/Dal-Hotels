ded08d6eba1738f8efe56785d1b99c82305a6ba9
iseoluwatobi agbola <is814889@dal.ca>
2019-06-18 11:35:16 -0300

    profile error fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ProfileComponent } from './profile/profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
