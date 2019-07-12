ceb2a5f15eb679af25d021d259b1ebd956ce6a65
William Devine <wl856147@dal.ca>
2019-07-12 13:20:17 -0300

    Added Firebase Firestore connections and implemented data retrival for available rooms and details

--- src/app/hotel-details/hotel-details.component.ts
+++ src/app/hotel-details/hotel-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AvaliableRoomServiceService } from '../avaliable-room-service.service';
 import { SearchFilterComponent } from '../search-filter/search-filter.component';
 import { AvaliableRoomService } from '../models/avaliable-room.service';
+import { AvailableRoom } from '../shared/available-room.model';
+
 
 @Component({
   selector: 'app-hotel-details',
@@ -10,23 +12,30 @@ import { AvaliableRoomService } from '../models/avaliable-room.service';
 })
 export class HotelDetailsComponent implements OnInit {
 
-  rooms:AvaliableRoomService[];
-  currentImg:number;
-	constructor(private roomService:AvaliableRoomServiceService) { }
+  rooms: AvailableRoom[];
+  currentImg: number;
 
-	ngOnInit() {
-		// this.rooms = this.roomService.getRooms();
-		this.roomService.getRooms().subscribe(rooms => {
-	  this.rooms = rooms;
-	  this.genRandomNum();
-      console.log(this.rooms);
-		});
-	}
+  constructor(private service: AvaliableRoomServiceService) {
+  }
+
+  ngOnInit() {
+    // this.rooms = this.roomService.getRooms();
+    this.service.getRooms().subscribe(actionArray => {
+      this.rooms = actionArray.map(item => {
+        return {
+          id: item.payload.doc.id,
+          ...item.payload.doc.data()
+        } as AvailableRoom;
+      });
+      // console.log(this.rooms);
+    });
+  }
 
 	genRandomNum() {
-	    this.currentImg = Math.floor(Math.random() * 6) + 1 ;
+    this.currentImg = Math.floor(Math.random() * 6) + 1;
 	}
-	changeImage(Image_number:number){
-		this.currentImg = Image_number;
-	}
+
+  changeImage(image_number: number) {
+    this.currentImg = image_number;
+  }
 }
